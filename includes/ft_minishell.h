6fdc370d90c3c9e28e415db4a7dce63181e85a85
rrhaenys <123yk123@mail.ru>
2019-07-16 19:47:22 +0300

    up start pack

--- includes/ft_minishell.h
+++ includes/ft_minishell.h
@@ -2,8 +2,12 @@
 #ifndef FT_MINISHELL_H
 # define FT_MINISHELL_H
 
+# define MAXDIR 100
+
 # include "ft_printf.h"
 # include <signal.h>
 # include <wait.h>
+// # include <dir.h>
+# include <stdio.h>
 
 #endif
--- src/ft_main.c
+++ src/ft_main.c
@@ -20,15 +20,55 @@ static int		run_cmd(char *path, char **args)
 	return (1);
 }
 
+void
+	curr_dir()
+{	
+	char dir[MAXDIR];
+
+	getcwd(dir, MAXDIR);
+	ft_printf("Current directory is %s\n", dir);
+    // using the command 
+    chdir("..");
+	getcwd(dir, MAXDIR);
+	ft_printf("Current directory is %s\n", dir);
+}
+
+void
+	sig_handler(int signo)
+{
+	if (signo == SIGINT)
+		ft_printf("\nrrhaenys:");
+	else
+		ft_printf("\nrrhaenys[%d]:", signo);
+}
+
+void
+	print_envp(char **envp)
+{
+	int	i;
+
+	i = -1;
+	while (envp[++i] != 0)
+	{
+		ft_printf("[%d](%s)\n", i, envp[i]);
+	}
+}
+
 int
-	main(int argc, char **argv)
+	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 
-	run_cmd("/bin/ls", argv + 1);
+	curr_dir();
+	print_envp(envp);
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		printf("\ncan't catch SIGINT\n");
+	run_cmd("/bin/ls", argv);
 	ft_putstr("rrhaenys:");
 	while (get_next_line(0, &line) == 1)
 	{
+		if (ft_strcmp(line, "exit") == 0)
+			exit(1);
 		ft_putendl(line);///bin/ls
 		ft_putstr("rrhaenys:");
 		free(line);
